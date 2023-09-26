b7a7e66eb965a64f6c510ac6a3d0e366a81130a9
Jens <jens@cetg.dk>
2023-09-26 14:54:56 +0200

    Præver at slippe arket

--- Excel.cs
+++ Excel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,57 +8,54 @@ using System.Text;
 using System.Threading.Tasks;
 using Excel = Microsoft.Office.Interop.Excel;       //microsoft Excel 14 object in references-> COM tab
 
-namespace ExcelSpace
-{
-    public class ExcelFileHandling
-    {
-        public static void getExcelFile()
-        {
+namespace ExcelSpace {
+    public class ExcelFileReading {
+        // Properties:
+        public string fileName;
 
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkbook;
+        Excel.Worksheet xlWorksheet;
+        Excel.Range xlRange;
+
+        // Constructor:
+        public ExcelFileReading(string fileName, Excel.Application application) {
             //Create COM Objects. Create a COM object for everything that is referenced
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"C:\VSC_PRO_B\VejlederMatrix\VejlederMatrix.xlsx");
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
+            xlApp = application;
+            xlWorkbook = xlApp.Workbooks.Open(fileName);
+        }
 
+        // Methods:
+        public List<string> getExcelColumn(int sheet, int col) {
+            xlWorksheet = xlWorkbook.Sheets[sheet];
+            xlRange = xlWorksheet.UsedRange;
             int rowCount = xlRange.Rows.Count;
             int colCount = xlRange.Columns.Count;
 
-            //iterate over the rows and columns and print to the console as it appears in the file
-            //excel is not zero based!!
-            for (int i = 1; i <= rowCount; i++)
-            {
-                for (int j = 1; j <= colCount; j++)
-                {
-                    //new line
-                    if (j == 1)
-                        Console.Write("\r\n");
-
-                    //write the value to the console
-                    if (xlRange.Cells[i, j] != null && xlRange.Cells[i, j].Value2 != null)
-                        Console.Write(xlRange.Cells[i, j].Value2.ToString() + "\t");
-                }
+            List<string> column = new List<string>();
+            for(int i = 1; i < rowCount; i++) {
+                column.Add(xlRange.Cells[i, col].Value2.ToString());
             }
+            return column;
+        }
+        public List<string> getExcelRow(int sheet, int row)
+        {
+            xlWorksheet = xlWorkbook.Sheets[sheet];
+            xlRange = xlWorksheet.UsedRange;
+            int colCount = xlRange.Columns.Count;
 
-            //cleanup
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
-
-            //rule of thumb for releasing com objects:
-            //  never use two dots, all COM objects must be referenced and released individually
-            //  ex: [somthing].[something].[something] is bad
-
-            //release com objects to fully kill excel process from running in the background
-            Marshal.ReleaseComObject(xlRange);
-            Marshal.ReleaseComObject(xlWorksheet);
-
-            //close and release
-            xlWorkbook.Close();
-            Marshal.ReleaseComObject(xlWorkbook);
-
-            //quit and release
-            xlApp.Quit();
-            Marshal.ReleaseComObject(xlApp);
+            List<string> column = new List<string>();
+            for (int i = 1; i < colCount; i++)
+            {
+                column.Add(xlRange.Cells[i, row].Value2.ToString());
+            }
+            return column;
+        }
+        public int[] Find(int sheet, string lookFor) {
+            int[] coords = new int[2];
+            coords[0] = xlWorkbook.Worksheets[sheet].UsedRange.Find(lookFor).Row;
+            coords[1] = xlWorkbook.Worksheets[sheet].UsedRange.Find(lookFor).Column;
+            return coords;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
 ﻿using ExcelSpace;
+using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Reflection.Metadata.BlobBuilder;
 
-ExcelFileHandling.getExcelFile();
+Excel.Application xlApp = new Excel.Application();
+
+string fileName = "C:\\VSC_PRO_B\\VejlederMatrix\\VejlederMatrix.xlsx";
+
+ExcelFileReading exFileReader  = new ExcelFileReading(fileName, xlApp);
+int[] coords = exFileReader.Find(1, "Vejleder1");
+
+Console.WriteLine($"Række: {coords[0]}\nKolonne: {coords[1]}");
+
+// Prøv at hoppe af appen:
+Excel.Workbooks books = null;
+Excel.Workbook book = null;
+Excel.Sheets sheets = null;
+Excel.Worksheet sheet = null;
+Excel.Range range = null;
+
+try
+{
+    books = xlApp.Workbooks;
+    book = books.Add();
+    sheets = book.Sheets;
+    sheet = sheets.Add();
+    range = sheet.Range["A1"];
+    range.Value = "Lorem Ipsum";
+    book.SaveAs(@"C:\Temp\ExcelBook" + DateTime.Now.Millisecond + ".xlsx");
+    book.Close();
+    xlApp.Quit();
+}
+finally
+{
+    if (range != null) Marshal.ReleaseComObject(range);
+    if (sheet != null) Marshal.ReleaseComObject(sheet);
+    if (sheets != null) Marshal.ReleaseComObject(sheets);
+    if (book != null) Marshal.ReleaseComObject(book);
+    if (books != null) Marshal.ReleaseComObject(books);
+    if (xlApp != null) Marshal.ReleaseComObject(xlApp);
+}
