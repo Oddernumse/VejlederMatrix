7014a69f75124527d3d4886e07c7395bd4abbc26
Jens <jens@cetg.dk>
2023-09-28 23:11:23 +0200

    Quite far now, has all the information it needs for the processing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using ExcelSpace;
 using Excel = Microsoft.Office.Interop.Excel;
-using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -30,15 +29,40 @@ int dataColumn1 = Int32.Parse(Console.ReadLine()) - 2;
 for(int i = 1; dataSheet.Cells[i, dataColumn1].Value2 != null || tilgængeligeLærere.Count == 0 ; i++)
 {
     object lærer1 = dataSheet.Cells[i, dataColumn1].Value2;
-    object lærer2 = dataSheet.Cells[i, dataColumn1 + 1].Value2;
-    int møder = Int32.Parse(dataSheet.Cells[i, dataColumn1].Value2.ToString());
     if (lærer1.ToString().Length <= 4)
     {
+        object lærer2 = dataSheet.Cells[i, dataColumn1 + 1].Value2;
+        string mødeString = dataSheet.Cells[i, dataColumn1 + 2].Value2.ToString();
+        int møder = Int32.Parse(mødeString);
         if (!tilgængeligeLærere.ContainsKey(lærer1.ToString())) tilgængeligeLærere.Add(lærer1.ToString(), true);
+        lærerPar.Add(new LærerPar(møder, lærer1.ToString(), lærer2.ToString()));
+    }
+}
+
+bool planlægning = false;
+for (int blok = 0; !planlægning; blok++)
+{
+    LærerPar emptyPar = new LærerPar(0, "", "");
+    planlægning = true;
+    bool blokDone = false;
+    while (!blokDone)
+    {
+        int maksMøder = 0;
+        int n = 0;
+        foreach (LærerPar par in lærerPar)
+        {
+            if (par.møder == 0) lærerPar.Remove(par);
+            else if (par.møder > maksMøder)
+            {
+                maksMøder = par.møder;
+                n = lærerPar.IndexOf(par);
+                planlægning = false;
+            }
+        }
+
     }
 }
 
 // Prøv at hoppe af appen:
-exFileHandler.Release();
 xlApp.Quit();
 Marshal.ReleaseComObject(xlApp);
