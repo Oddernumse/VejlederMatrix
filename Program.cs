835d17cef09b3ecd06379c7d1da2dea85cc86353
Jens <jens@cetg.dk>
2023-11-27 12:40:54 +0100

    Brug Rust istedet i guess

--- Program.cs
+++ Program.cs
@@ -154,3 +154,6 @@ foreach (Excel.Workbook book in xlApp.Workbooks)
 }
 xlApp.Quit();
 Marshal.ReleaseComObject(xlApp);
+
+Console.WriteLine("Tryk på en knap for at afslutte programmet");
+Console.ReadKey();
